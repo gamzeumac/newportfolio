32e84ee74dfe5995259e552c24e06d9d272220db
Gamze <umacgamze49@gmail.com>
2021-11-12 22:22:00 +0100

    portfolio is ok

---
+++ src/data/BlogData.js
@@ -0,0 +1,36 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"How can i use React useFormik with Modals",
+        tags:["react","formik"],
+        date:"30 Sep, 2021",
+        imgSrc:"https://photos.google.com/share/AF1QipNLh-doYSnOSzoOnZ_W_JiTUqATY951D-Lx7AWQH_pMdnTKFjdbOJqZqtPXI6qMIQ/photo/AF1QipM7pfKXSckrJMP4Mq_SyFIFvQBnTbDuFhP6TtG8?key=RkFsNjVlQTEyRTd5X3loTVhVcXowWmJDcXoxcU5R",
+        link:"https://medium.com/@umacgamze49/how-can-i-use-react-useformik-with-modals-54e0f229dbbe"
+    },
+    {
+        id:2,
+        name:"Axios or fetch? What is the difference?",
+        tags:["react","node-fetch"],
+        date:"8 Oct, 2021",
+        imgSrc:"https://photos.google.com/share/AF1QipNLh-doYSnOSzoOnZ_W_JiTUqATY951D-Lx7AWQH_pMdnTKFjdbOJqZqtPXI6qMIQ/photo/AF1QipPDrNKmVZTlaQM5IDm6xP4qZgGklCOOmJ2S7DD2?key=RkFsNjVlQTEyRTd5X3loTVhVcXowWmJDcXoxcU5R",
+        link:"https://medium.com/@umacgamze49/axios-or-fetch-what-is-the-difference-e7c727f4f0a8"
+    },
+    {
+        id:3,
+        name:"About Kubernetes and Docker",
+        tags:["kubernetes","docker"],
+        date:"6 July, 2021",
+        imgSrc:"https://photos.google.com/share/AF1QipNLh-doYSnOSzoOnZ_W_JiTUqATY951D-Lx7AWQH_pMdnTKFjdbOJqZqtPXI6qMIQ/photo/AF1QipOv4ONldfMRXWbDjo6cNK9iOY7r5eHm9GpaBQQA?key=RkFsNjVlQTEyRTd5X3loTVhVcXowWmJDcXoxcU5R",
+        link:"https://medium.com/@umacgamze49/about-kubernetes-and-docker-f2331556ec71"
+    },
+    {
+        id:4,
+        name:"JWM, JRE AND JDK",
+        tags:["java","Java Runtime Environment"],
+        date:"7 Nov, 2021",
+        imgSrc:"https://photos.google.com/share/AF1QipNLh-doYSnOSzoOnZ_W_JiTUqATY951D-Lx7AWQH_pMdnTKFjdbOJqZqtPXI6qMIQ/photo/AF1QipN1dn7Ixddz8rcR_G34i8ZJook-tUrgjouIr7i7?key=RkFsNjVlQTEyRTd5X3loTVhVcXowWmJDcXoxcU5R",
+        link:"https://medium.com/@umacgamze49/jwm-jre-and-jdk-f93449c97055"
+    } 
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,84 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Quiz App",
+        description:"It is build on top of the React JS, with Material-UI.",
+        tags:["react","Material-UI", "Netlify"],
+        
+        demo:"https://mystartquiz.netlify.app/",
+        github:"https://github.com/gamzeumac/startquiz"
+    },
+    {
+        id:2,
+        name:"Recipe",
+        description:"A Recipe listing website build in ReactJS. With realtime fetching of articles from dev.to using API.",
+        tags:["react","sass", "Github-Pages"],
+        
+        demo:"https://gamzeumac.github.io/api/",
+        github:"https://github.com/gamzeumac/api"
+        
+    },
+    { id:3,
+        name:"Music Player",
+        description:"A Music Player build using JS  ",
+        tags:["JS","HTML","CSS"],
+        
+        demo:"https://gamzeumac.github.io/MusicPlayer/",
+        github:"https://github.com/gamzeumac/MusicPlayer"
+        
+
+    }
+    
+   ,
+    {
+        id:4,
+        name:"Restaurant Page",
+        description:"A cool looking Restaurant build using JS and Html.",
+        tags:["html","css","js"],
+        
+        demo:"https://myrestaurantpage.netlify.app/",
+        github:"https://github.com/gamzeumac/myRestauranPagewithJS"
+       
+    },{
+        id:5,
+        name:"Todo App(Typescript + Redux)",
+        description:"A todo app build using Redux-toolkit and Typescript for smooth animations.",
+        tags:["react","redux","typescript"],
+        
+        demo:"https://todo-withtypescript.netlify.app/",
+        github:"https://github.com/gamzeumac/todowithtypescript"
+        
+    },
+    {
+        id:6,
+        name:"QR Scanner",
+        description:"A responsive QR Scanner component in ReactJS with Material-Ui functionalities. Built from scratch.",
+        tags:["react","Material-UI"],
+        
+        demo:"https://my-qr-scanner.netlify.app/",
+        github:"https://github.com/gamzeumac/my_qr_scanner"
+        
+    },
+    { id:7,
+        name:"Todo App(VueJS + Bootstrap)",
+        description:"A todo app build using VueJS(CLI) and Bootstrap for smooth animations.",
+        tags:["VueJS","Bootstrap","typescript"],
+        
+        demo:"https://mytodowithvuejs.netlify.app/",
+        github:"https://github.com/gamzeumac/Todo_with_VueJS"
+        
+
+    }, {
+        id:8,
+        name:"Music App",
+        description:"A Music App build using JS and HTML ",
+        tags:["JS","Html","Css"],
+        
+        demo:"https://gamzeumac.github.io/MusicApp/",
+        github:"https://github.com/gamzeumac/MusicApp"
+    }
+  
+  
+] 
