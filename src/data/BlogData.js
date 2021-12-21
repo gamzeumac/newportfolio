26f35f8acda1d858d8c1093b4fbcd7e3a73b9d19
Gamze <umacgamze49@gmail.com>
2021-12-21 20:16:47 +0100

    images bugfix ist fertig

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -6,7 +6,7 @@ export const Blogs = [
         name:"How can i use React useFormik with Modals",
         tags:["react","formik"],
         date:"30 Sep, 2021",
-        imgSrc:"https://photos.google.com/share/AF1QipNLh-doYSnOSzoOnZ_W_JiTUqATY951D-Lx7AWQH_pMdnTKFjdbOJqZqtPXI6qMIQ/photo/AF1QipM7pfKXSckrJMP4Mq_SyFIFvQBnTbDuFhP6TtG8?key=RkFsNjVlQTEyRTd5X3loTVhVcXowWmJDcXoxcU5R",
+        imgSrc:"/images/reactmodal.png",
         link:"https://medium.com/@umacgamze49/how-can-i-use-react-useformik-with-modals-54e0f229dbbe"
     },
     {
@@ -14,7 +14,7 @@ export const Blogs = [
         name:"Axios or fetch? What is the difference?",
         tags:["react","node-fetch"],
         date:"8 Oct, 2021",
-        imgSrc:"https://photos.google.com/share/AF1QipNLh-doYSnOSzoOnZ_W_JiTUqATY951D-Lx7AWQH_pMdnTKFjdbOJqZqtPXI6qMIQ/photo/AF1QipPDrNKmVZTlaQM5IDm6xP4qZgGklCOOmJ2S7DD2?key=RkFsNjVlQTEyRTd5X3loTVhVcXowWmJDcXoxcU5R",
+        imgSrc:"/images/axios.png",
         link:"https://medium.com/@umacgamze49/axios-or-fetch-what-is-the-difference-e7c727f4f0a8"
     },
     {
@@ -22,7 +22,7 @@ export const Blogs = [
         name:"About Kubernetes and Docker",
         tags:["kubernetes","docker"],
         date:"6 July, 2021",
-        imgSrc:"https://photos.google.com/share/AF1QipNLh-doYSnOSzoOnZ_W_JiTUqATY951D-Lx7AWQH_pMdnTKFjdbOJqZqtPXI6qMIQ/photo/AF1QipOv4ONldfMRXWbDjo6cNK9iOY7r5eHm9GpaBQQA?key=RkFsNjVlQTEyRTd5X3loTVhVcXowWmJDcXoxcU5R",
+        imgSrc:"/images/docker.png",
         link:"https://medium.com/@umacgamze49/about-kubernetes-and-docker-f2331556ec71"
     },
     {
@@ -30,7 +30,7 @@ export const Blogs = [
         name:"JWM, JRE AND JDK",
         tags:["java","Java Runtime Environment"],
         date:"7 Nov, 2021",
-        imgSrc:"https://photos.google.com/share/AF1QipNLh-doYSnOSzoOnZ_W_JiTUqATY951D-Lx7AWQH_pMdnTKFjdbOJqZqtPXI6qMIQ/photo/AF1QipN1dn7Ixddz8rcR_G34i8ZJook-tUrgjouIr7i7?key=RkFsNjVlQTEyRTd5X3loTVhVcXowWmJDcXoxcU5R",
+        imgSrc:"/images/java.png",
         link:"https://medium.com/@umacgamze49/jwm-jre-and-jdk-f93449c97055"
     } ,
     ,
@@ -39,7 +39,7 @@ export const Blogs = [
         name:"Ist NoSQL schneller als SQL?",
         tags:["SQL","NoSQL, Database"],
         date:"21 Dez, 2021",
-        imgSrc:"",
+        imgSrc:"/images/sql.png",
         link:"https://medium.com/@umacgamze49/ist-nosql-schneller-als-sql-aed13987fb61"
     } 
 ] 
